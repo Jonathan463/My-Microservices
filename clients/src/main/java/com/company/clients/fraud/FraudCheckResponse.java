79a6a2be2871c417d1e974a6d4dafb57e5f8036c
decagon <godsonjonathan39@gmail.com>
2022-04-06 19:39:00 +0100

    Configure Feign

---
+++ clients/src/main/java/com/company/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,9 @@
+package com.company.clients.fraud;
+
+public class FraudCheckResponse {
+    private final Boolean isFraudster;
+
+    public FraudCheckResponse(Boolean isFraudster) {
+        this.isFraudster = isFraudster;
+    }
+}
