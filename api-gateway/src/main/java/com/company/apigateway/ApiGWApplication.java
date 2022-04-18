aac395915e351a69533ca25755dc8ac62fafdc68
decagon <godsonjonathan39@gmail.com>
2022-04-18 23:43:40 +0100

    Set up Application.yaml for Api gatewate

--- api-gateway/src/main/java/com/company/apigateway/ApiGWApplication.java
+++ api-gateway/src/main/java/com/company/apigateway/ApiGWApplication.java
@@ -2,7 +2,9 @@ package com.company.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiGWApplication {
     public static void main(String[] args){
